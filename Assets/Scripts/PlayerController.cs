db2fd83383566baaaba4d218ec756b08842337f1
Ridwan Satria Wicaksana <wicaksana.rs@gmail.com>
2023-01-10 12:08:09 +0700

    add level by rdwan

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     public float force;
     private float position;
+    private Vector2 location;
     private bool isJump = false;
     public GameObject map;
 
@@ -16,38 +17,53 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         position = Portal.position;
         Debug.Log("Ini" + position);
+        location = rb.position;
+        Debug.Log("Ini" + location);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0) && !isJump){
+        // if(Input.GetMouseButton(0) && !isJump){
+        //     isJump = true;
+        //     rb.AddForce(new Vector2(0, force));
+        //     transform.Rotate(0, 0, 180);
+        // }
+if(!isJump){
+    //    if(Input.GetMouseButton(0)){
+       if(Input.GetKey(KeyCode.Space)){
+            isJump = true;
             rb.AddForce(new Vector2(0, force));
             transform.Rotate(0, 0, 180);
-            isJump = true;
         }
+}
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if(isJump){
-            isJump = false;
-        }
+        // if(isJump){
+        //     isJump = false;
+        // }
 
         if(collision.transform.tag.Equals("Ground")){
-            rb.freezeRotation = true;
+            rb.freezeRotation = true;   
+            isJump = false;       
         }
 
         if(collision.transform.tag.Equals("Obstacle")){
             map.transform.position = new Vector2(transform.position.x, map.transform.position.y);
+            rb.transform.position = location;
+            // yield WaitForSeconds (3.0);
+            // yield return new WaitForSeconds(5);
         }
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.transform.tag.Equals("Jumper")){
             rb.AddForce(new Vector2(0, 2000));
             isJump = true;
         }
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.transform.tag.Equals("PortalHorizontal")){
             MapMover.speed = -MapMover.speed;
             map.transform.position = new Vector2(position, map.transform.position.y);
